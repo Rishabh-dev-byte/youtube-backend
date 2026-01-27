f885892b457aa18089e55628002a45c26fc07b53
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-01-28 00:30:09 +0530

    refactor: updated playlist controller and routes for improved error handling and consistency

--- src/controller/playlist.controller.js
+++ src/controller/playlist.controller.js
@@ -1,5 +1,5 @@
 
-import mongoose, {isValidObjectId} from "mongoose"
+import mongoose from "mongoose"
 import {Playlist} from "../models/playlist.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
@@ -13,40 +13,117 @@ const createPlaylist = asyncHandler(async (req, res) => {
         throw new ApiError(404,"all fields are required for playlist")
     }
 
-
-
-   
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner:req.user._id
+    })
+    
+    res.status(200).json(new ApiResponse(200,playlist,"playlist created successfully"))
+  
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
-    //TODO: get user playlists
+
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+        throw new ApiError(400,"userId for userplaylist is missing")
+    }
+
+    const userPlaylist = await Playlist.find({owner:userId})
+
+    return res.status.json(new ApiResponse(200,userPlaylist,"playlists by user fetched successfully"))
+   
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    //TODO: get playlist by id
+    
+    if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400,"userId for userplaylist is missing")
+    }
+
+    const playlistById = await Playlist.findById(playlistId)
+
+    return res.status.json(new ApiResponse(200,playlistById,"playlists by id fetched successfully"))
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
+    const {playlistId, videoId} = req.query
+    
+    if(!mongoose.Types.ObjectId.isValid(playlistId) || !mongoose.Types.ObjectId.isValid(videoId)){
+        throw new ApiError(404,"id missing for adding video to playlist")
+    }
+    
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $addToSet:{videos:videoId}
+        },
+        {new:true}
+    )
+
+    return res.status(200).json(new ApiResponse(200,playlist,"video added to playlist successfully"))
+
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+    
+     if(!mongoose.Types.ObjectId.isValid(playlistId) || !mongoose.Types.ObjectId.isValid(videoId)){
+        throw new ApiError(404,"id missing for removing video to playlist")
+    }
+    
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull:{videos:videoId}
+        },
+        {new:true}
+    )
+
+    return res.status(200).json(new ApiResponse(200,playlist,"video removed from playlist successfully"))
 
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    // TODO: delete playlist
+
+     if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400,"userId for deleting playlist is missing")
+    }
+    
+    const deletedPlaylist = await Playlist.findOneAndDelete({_id:playlistId})
+
+    return res.status(200).json(new ApiResponse(200,deletedPlaylist,"playlist deleted successfully"))
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
-    //TODO: update playlist
+
+     if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new ApiError(400,"userId for deleting playlist is missing")
+    }
+    
+     if(!name || !description){
+        throw new ApiError(404,"all fields are required for playlist")
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set:{
+                name:name,
+                description:description
+            }
+        },
+        {new:true}
+    )
+    
+    return res.status(200).json(new ApiResponse(200,updatedPlaylist,"playlist updated successfully"))
+
 })
 
 export {
--- src/routes/playlist.route.js
+++ src/routes/playlist.route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { Router } from 'express';
+
 import {
     addVideoToPlaylist,
     createPlaylist,
@@ -8,12 +8,19 @@ import {
     getUserPlaylists,
     removeVideoFromPlaylist,
     updatePlaylist,
-} from "../controllers/playlist.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+} from "../controller/playlist.controller.js"
+
 
 const router = Router();
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
-const router = Router();
+router.route("/createPlaylist").get(verifyJWT,createPlaylist)
+router.route("/getUserPlaylists/:userId").get(verifyJWT,getUserPlaylists)
+router.route("/getPlaylistById/:playlistId").get(verifyJWT,getPlaylistById)
+router.route("/addVideoToPlaylist").get(verifyJWT,addVideoToPlaylist)
+router.route("/removeVideoFromPlaylist").get(verifyJWT,removeVideoFromPlaylist)
+router.route("/deletePlaylist/:playlistId").get(verifyJWT,deletePlaylist)
+router.route("/updatePlaylist/:playlistId").post(verifyJWT,updatePlaylist)
+
 
 export default router
