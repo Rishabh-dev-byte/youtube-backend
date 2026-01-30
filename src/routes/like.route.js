14a54dae9cc9b707b696689c2f03eb5dfa02d8ed
Rishabh Tiwari <rishabhtiwari3479@gmail.com>
2026-01-31 01:12:19 +0530

    added comment model, routes, and like functionality for comments

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -9,5 +9,8 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 router.route("/toggleVideoLike/:videoId").get(verifyJWT,toggleVideoLike)
+router.route("/toggleCommentLike/:commentId").get(verifyJWT,toggleCommentLike)
+router.route("/toggleTweetLike/:tweetId").get(verifyJWT,toggleTweetLike)
+router.route("/getLikedVideos").get(verifyJWT,getLikedVideos)
 
 export default router
